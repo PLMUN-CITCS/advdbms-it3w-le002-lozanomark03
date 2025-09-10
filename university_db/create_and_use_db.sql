42e4f8fd29523dc27222ed387b7bcaf96e3dbb22
lozanomark03 <lozanomark_bsit@plmun.edu.ph>
2025-09-10 10:54:36 +0800

    Submitting ADVDBMS Week 04 - Session 01 - Exercise 02

---
+++ university_db/create_and_use_db.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE `UniversityDB`;
+-- Step 2: Use the newly created database
+USE `UniversityDB`;
+
+-- Verification (Optional): 
+-- You can add a simple query to verify the database is selected.
+SELECT DATABASE(); -- This will show the currently selected database.
